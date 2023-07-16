8e0344adf733ebb10a2d9b85eb1233f7adc8fcd4
ewsn0825 <ewsn0825@naver.com>
2023-07-16 22:20:56 +0900

    자바스크립트 과제 코드 구현 및 리드미 작성

--- mission01/naver_login/README.md
+++ mission01/naver_login/README.md
@@ -3,16 +3,121 @@
 
 ---
 
-로그인과 비밀번호를 정확히 입력했을 때 welcome 페이지로 넘어갈 수 있도록 코드 로직을 작성합니다.
+## 🔒 조건 
+1. email 정규표현식을 사용한 validation
+2. pw 정규표현식을 사용한 validation
+3. 상태 변수 관리
+4. 로그인 버튼을 클릭시 user.id의 값과 input의 값을 비교
+5. 로그인 버튼을 클릭시 user.pw의 값과 input의 값을 비교
+6. 두 값이 일치 한다면 다음 페이지(welcome.html)로 이동
+
+<br><br>
+## 📌 변경사항
+- button의 type을 submit에서 button으로 변경하였습니다.
 
 
----
-- [x] 재사용 가능한 함수를 분리하고 함수를 중심으로 설계하는 방법에 대해 학습합니다.
+<br><br>
+## 👨‍💻 구현
+<br>
+### 초기 변수 설정
+```js
 
+//dom에 접근
+const email = document.querySelector(".user-email-input");
+const pw = document.querySelector(".user-password-input");
+const button = document.querySelector(".btn-login");
 
+let emailPass = false;
+let pwPass = false;
+```
+<br>
+### 1. email 정규표현식을 사용한 validation
+```js
+//이메일 validation
+email.addEventListener("input", function () {
+  if (emailReg(this.value)) {
+    this.classList.remove("is-active");
+    emailPass = true;
+  } else {
+    this.classList.add("is-active");
+    emailPass = false;
+    throw new Error("아이디는 이메일 형식으로 입력해주세요.");
+  }
+});
+```
 
+<br>
+### 2. pw 정규표현식을 사용한 validation
+```js
+//패스워드 validation
+pw.addEventListener("input", function () {
+  if (pwReg(this.value)) {
+    this.classList.remove("is-active");
+    pwPass = true;
 
+  } else {
+    this.classList.add("is-active");
+    pwPass = false;
+    throw new Error("비밀번호는 특수문자 포함 6자리 이상, 16자리 이하로 입력해주세요.");
+  }
 
+});
+```
 
+<br>
+### 3. 상태 변수 관리
+```js
+let emailPass = false;
+let pwPass = false;
 
+//validation true시에 emailPass, pwPass에 true 할당
+
+```
+
+<br>
+### 4. 로그인 버튼을 클릭시 user.id의 값과 input의 값을 비교
+```js
+
+//아이디가 일치하지 않았을 때
+    if (emailPass&&(email.value !== user.id)) {
+      throw new Error("아이디가 잘못되었습니다.");
+    }
+
+```
+
+<br>
+### 5. 로그인 버튼을 클릭시 user.pw의 값과 input의 값을 비교
+```js
+//비밀번호가 일치하지 않았을 때
+    if (pwPass && (pw.value !== user.pw)) {
+      throw new Error("비밀번호가 잘못되었습니다.");
+    }
+
+```
+
+<br>
+### 6. 두 값이 일치 한다면 다음 페이지(welcome.html)로 이동
+```js
+
+button.addEventListener("click", () => {
+
+  //아이디 비밀번호 비교 true일 경우 페이지 이동
+  if (email.value === user.id && pw.value === user.pw) {
+    console.log('로그인되었습니다.')
+    return (window.location.href = "welcome.html");
+  }
+
+  else{
+
+    //아이디가 일치하지 않았을 때
+    if (emailPass&&(email.value !== user.id)) {
+      throw new Error("아이디가 잘못되었습니다.");
+    }
+
+    //비밀번호가 일치하지 않았을 때
+    if (pwPass && (pw.value !== user.pw)) {
+      throw new Error("비밀번호가 잘못되었습니다.");
+    }
+  }
+```
 
--- mission01/naver_login/js/main.js
+++ mission01/naver_login/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: "asd@naver.com",
+  pw: "spdlqj123!@",
+};
 
 /*
 
@@ -13,24 +12,96 @@ const user = {
 
 */
 
-function emailReg(text){
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//dom에 접근
 
-  return re.test(String(text).toLowerCase())
+// const input=document.querySelector('#todo');
+// console.log(input.value);
+
+//input 실시간 이벤트
+// let value=input.value
+// input.addEventListener('input',()=>{
+//   value=input.value;
+//   console.log(input.value);
+// })
+
+// input.classList.add('is-active');
+// input.classList.remove('is-active');
+
+//토글
+//classList.toggle()
+
+function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
 
 
+//-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+const email = document.querySelector(".user-email-input");
+const pw = document.querySelector(".user-password-input");
+const button = document.querySelector(".btn-login");
+
+let emailPass = false;
+let pwPass = false;
 
 
+//이메일 validation
+email.addEventListener("input", function () {
+  if (emailReg(this.value)) {
+    this.classList.remove("is-active");
+    emailPass = true;
+  } else {
+    this.classList.add("is-active");
+    emailPass = false;
+    throw new Error("아이디는 이메일 형식으로 입력해주세요.");
+  }
 
+});
 
+//패스워드 validation
+pw.addEventListener("input", function () {
+  if (pwReg(this.value)) {
+    this.classList.remove("is-active");
 
+    pwPass = true;
+  } else {
+    this.classList.add("is-active");
+    pwPass = false;
+    throw new Error("비밀번호는 특수문자 포함 6자리 이상, 16자리 이하로 입력해주세요.");
+  }
 
+});
 
+button.addEventListener("click", () => {
+  
+  //아이디 비밀번호 비교
+  if (email.value === user.id && pw.value === user.pw) {
+    console.log('로그인되었습니다.')
+    return (window.location.href = "welcome.html");
+  }
 
+  else{
+
+    //아이디가 일치하지 않았을 때
+    if (emailPass&&(email.value !== user.id)) {
+      throw new Error("아이디가 잘못되었습니다.");
+    }
+  
+    //비밀번호가 일치하지 않았을 때
+    if (pwPass && (pw.value !== user.pw)) {
+      throw new Error("비밀번호가 잘못되었습니다.");
+    }
+  }
+
+  
+});
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
